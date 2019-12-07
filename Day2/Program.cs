4445f0071d8de008428c34fcd21b8c5a8837fef3
Jonathan Axelsson <jonathan.axelsson@sogeti.com>
2019-12-07 20:22:16 +0100

    Car coding best coding

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+
 
 namespace Day2
 {
@@ -6,7 +8,60 @@ namespace Day2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllText("input.txt").Split(',');
+
+            int[] intCodes = new int[input.Length];
+            int[] intOrig = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                intCodes[i] = int.Parse(input[i]);
+                intOrig[i] = int.Parse(input[i]);
+            }
+
+            int result = 0;
+            for (int noun = 0; noun < 100; noun++)
+            {
+                for (int verb = 0; verb < 100; verb++)
+                {
+                    intCodes = (int[])intOrig.Clone();
+                    result = OutputLocator(noun, verb, intCodes);
+                    if (result == 19690720)
+                    {
+                        Console.WriteLine(100 * intCodes[1] + intCodes[2]);
+                        Console.ReadKey();
+                    }
+                }
+            }
+
+        }
+
+        static int OutputLocator(int noun, int verb, int[] intCodes)
+        {
+            intCodes[1] = noun;
+            intCodes[2] = verb;
+
+            int pos = 0;
+            while (intCodes[pos] != 99)
+            {
+                intCodes = ProcessOpCode(intCodes[pos], pos, intCodes);
+                pos += 4;
+            }
+
+            return intCodes[0];
+        }
+
+        static int[] ProcessOpCode(int opCode, int pos, int[] intCodes)
+        {
+            if (opCode == 1)
+            {
+                intCodes[intCodes[pos + 3]] = intCodes[intCodes[pos + 1]] + intCodes[intCodes[pos + 2]];
+            }
+            if (opCode == 2)
+            {
+                intCodes[intCodes[pos + 3]] = intCodes[intCodes[pos + 1]] * intCodes[intCodes[pos + 2]];
+            }
+            return intCodes;
         }
     }
 }
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Day5
 {
@@ -6,7 +7,110 @@ namespace Day5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllText("input.txt").Split(',');
+
+            int[] intCodes = new int[input.Length];
+            int[] intOrig = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                intCodes[i] = int.Parse(input[i]);
+            }
+
+            OutputLocator(intCodes);
+        }
+
+        static void OutputLocator(int[] intCodes)
+        {
+            int pos = 0;
+            int mode = 0;
+            int paramMode1 = 0;
+            int paramMode2 = 0;
+            int paramMode3 = 0;
+            int param1 = 0;
+            int param2 = 0;
+
+
+            while (intCodes[pos] != 99)
+            {
+                var instr = intCodes[pos].ToString().PadLeft(5, '0');
+                mode = int.Parse(instr.Substring(3, 2));
+                paramMode1 = int.Parse(instr.Substring(2, 1));
+                paramMode2 = int.Parse(instr.Substring(1, 1));
+                paramMode3 = int.Parse(instr.Substring(0, 1));
+
+                if (mode == 1 || mode == 2)
+                {
+                    param1 = paramMode1 == 0 ? intCodes[intCodes[pos + 1]] : intCodes[pos + 1];
+                    param2 = paramMode2 == 0 ? intCodes[intCodes[pos + 2]] : intCodes[pos + 2];
+                }
+
+                switch (mode)
+                {
+                    case 1:
+
+                        intCodes[intCodes[pos + 3]] = param1 + param2;
+                        pos += 4;
+                        break;
+                    case 2:
+                        intCodes[intCodes[pos + 3]] = param1 * param2;
+                        pos += 4;
+                        break;
+                    case 3:
+                        Console.WriteLine("Input: ");
+                        var input = int.Parse(Console.ReadLine());
+                        intCodes[intCodes[pos + 1]] = input;
+                        pos += 2;
+                        break;
+                    case 4:
+                        var output = intCodes[intCodes[pos + 1]];
+                        Console.WriteLine($"Output: {output}");
+                        pos += 2;
+                        break;
+                    case 5:
+                        if (intCodes[pos + 1] != 0)
+                        {
+                            intCodes[pos] = intCodes[pos + 2];
+                        }
+                        else
+                            pos += 3;
+                        break;
+                    case 6:
+                        if (intCodes[pos + 1] == 0)
+                        {
+                            intCodes[pos] = intCodes[pos + 2];
+                        }
+                        else
+                            pos += 3;
+                        break;
+                    case 7:
+                        if (intCodes[pos + 1] < intCodes[pos + 2])
+                        {
+                            intCodes[intCodes[pos + 3]] = 1;
+                        }
+                        else
+                        {
+                            intCodes[intCodes[pos + 3]] = 0;
+                            pos += 4;
+                        }
+                        break;
+                    case 8:
+                        if (intCodes[pos + 1] == intCodes[pos + 2])
+                        {
+                            intCodes[intCodes[pos + 3]] = 1;
+                        }
+                        else
+                        {
+                            intCodes[intCodes[pos + 3]] = 0;
+                            pos += 4;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Fel");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
     }
 }
